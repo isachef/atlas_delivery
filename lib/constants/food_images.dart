5f1bfe6b3b28f8094df5a44eed87a5d918e08e35
isachef <isa.smail1123@gmail.com>
2026-05-14 05:15:45 +0600

    new feature

--- lib/constants/food_images.dart
+++ lib/constants/food_images.dart
@@ -1,143 +1,63 @@
 class FoodImages {
-  static const String _basePath = 'https://images.unsplash.com/photo-';
+  // Изображения ресторанов
+  static String mcdonalds = 'assets/images/restaurants/mcdonalds.png';
+  static String dodoPizza = 'assets/images/restaurants/dodo_pizza.png';
+  static String sushiMaster = 'assets/images/restaurants/sushi_hero.png';
+  static String kfc = 'assets/images/restaurants/kfc.png';
+  static String restaurant = 'assets/images/restaurants/restaurant_hero.jpg';
+  static String faiza = 'assets/images/restaurants/faiza.png';
 
-  static String burger1 =
-      '${_basePath}1568901346375-23c9450c58cd?w=500&auto=format';
-  static String burger2 =
-      '${_basePath}1550317138-6a8b75f3c4a3?w=500&auto=format';
-  static String burger3 =
-      '${_basePath}1550547660-d9450f859349?w=500&auto=format';
-  static String burger4 =
-      '${_basePath}1586190848861-99aa4a171e90?w=500&auto=format';
+  // Изображения категорий блюд
+  static String burger = 'assets/images/food/categories/burger.jpg';
+  static String pizza = 'assets/images/food/categories/pizza.jpg';
+  static String sushi = 'assets/images/food/categories/sushi.jpg';
+  static String fries = 'assets/images/food/categories/chicken.jpeg';
+  static String drink = 'assets/images/food/categories/drink.jpg';
+  static String dessert = 'assets/images/food/categories/dessert.jpg';
 
-  static String pizza1 =
-      '${_basePath}1565299624946-b28f40a0ae38?w=500&auto=format';
-  static String pizza2 =
-      '${_basePath}1593504049359-74330189a345?w=500&auto=format';
-  static String pizza3 =
-      '${_basePath}1628840042765-356cda07367e?w=500&auto=format';
-  static String pizza4 =
-      '${_basePath}1513104890138-7c749659a591?w=500&auto=format';
+  static String getImageForRestaurant(String restaurantName) {
+    final name = restaurantName.toLowerCase();
+    if (name.contains('макдоналдс')) return mcdonalds;
+    if (name.contains('додо') || name.contains('пицца')) return dodoPizza;
+    if (name.contains('суши')) return sushiMaster;
+    if (name.contains('kfc')) return kfc;
+    if (name.contains('фаиза')) return faiza;
+    return restaurant;
+  }
 
-  static String sushi1 =
-      '${_basePath}1553621042-f6e147245754?w=500&auto=format';
-  static String sushi2 =
-      '${_basePath}1579871494447-9811cf80d66c?w=500&auto=format';
-  static String sushi3 =
-      '${_basePath}1617196034796-73dfa7b1fb52?w=500&auto=format';
-  static String sushi4 =
-      '${_basePath}1583623025453-7fc93c9a4882?w=500&auto=format';
-
-  static String salad1 =
-      '${_basePath}1546069901-ba9599a7e63c?w=500&auto=format';
-  static String salad2 =
-      '${_basePath}1540420773420-3366772f4999?w=500&auto=format';
-  static String salad3 =
-      '${_basePath}1505253716362-36a40f8523fa?w=500&auto=format';
-  static String salad4 =
-      '${_basePath}1564675803565-4395a9a2c4d3?w=500&auto=format';
-
-  static String pasta1 =
-      '${_basePath}1627308595229-7830a5c91f9f?w=500&auto=format';
-  static String pasta2 =
-      '${_basePath}1556761223-4c60b45f51cd?w=500&auto=format';
-  static String pasta3 =
-      '${_basePath}1563379926898-05f4575a45d8?w=500&auto=format';
-  static String pasta4 =
-      '${_basePath}1473093295043-cdd812d0e601?w=500&auto=format';
-
-  static String dessert1 =
-      '${_basePath}1587314168485-3236d6710814?w=500&auto=format';
-  static String dessert2 =
-      '${_basePath}1551024709-8f23befc6f87?w=500&auto=format';
-  static String dessert3 =
-      '${_basePath}1488477181946-6428a0291777?w=500&auto=format';
-  static String dessert4 =
-      '${_basePath}1563805042-7684c019e1cb?w=500&auto=format';
-
-  static String drink1 =
-      '${_basePath}1544145945-f9347d4e26a8?w=500&auto=format';
-  static String drink2 =
-      '${_basePath}1513558161293-cdaf765ed2fd?w=500&auto=format';
-  static String drink3 =
-      '${_basePath}1527661650979-e04638a3a171?w=500&auto=format';
-  static String drink4 =
-      '${_basePath}1497534446932-c925b458314e?w=500&auto=format';
-
-  static String breakfast1 =
-      '${_basePath}1533089860892-a886cc845dbc?w=500&auto=format';
-  static String breakfast2 =
-      '${_basePath}1533089860892-a886cc845dbc?w=500&auto=format';
-  static String breakfast3 =
-      '${_basePath}1494597564530-0bc83d4237d4?w=500&auto=format';
-  static String breakfast4 =
-      '${_basePath}1484723091739-30a097e8f929?w=500&auto=format';
-
-  static String getRandomForCategory(String category) {
+  static String getImageForCategory(String category) {
     switch (category.toLowerCase()) {
       case 'burger':
       case 'бургер':
       case 'бургеры':
-        return [burger1, burger2, burger3, burger4][DateTime.now().microsecond %
-            4];
+        return burger;
       case 'pizza':
       case 'пицца':
       case 'пиццы':
-        return [pizza1, pizza2, pizza3, pizza4][DateTime.now().microsecond % 4];
+        return pizza;
       case 'sushi':
       case 'суши':
       case 'роллы':
-        return [sushi1, sushi2, sushi3, sushi4][DateTime.now().microsecond % 4];
-      case 'salad':
-      case 'салат':
-      case 'салаты':
-        return [salad1, salad2, salad3, salad4][DateTime.now().microsecond % 4];
-      case 'pasta':
-      case 'паста':
-      case 'макароны':
-        return [pasta1, pasta2, pasta3, pasta4][DateTime.now().microsecond % 4];
+        return sushi;
+      case 'fries':
+      case 'картофель':
+      case 'картофель фри':
+      case 'закуски':
+        return fries;
       case 'dessert':
       case 'десерт':
       case 'десерты':
-        return [
-          dessert1,
-          dessert2,
-          dessert3,
-          dessert4,
-        ][DateTime.now().microsecond % 4];
+        return dessert;
       case 'drink':
       case 'напиток':
       case 'напитки':
-        return [drink1, drink2, drink3, drink4][DateTime.now().microsecond % 4];
-      case 'breakfast':
-      case 'завтрак':
-      case 'завтраки':
-        return [
-          breakfast1,
-          breakfast2,
-          breakfast3,
-          breakfast4,
-        ][DateTime.now().microsecond % 4];
+        return drink;
       default:
-        final allImages = [
-          burger1,
-          burger2,
-          pizza1,
-          pizza2,
-          sushi1,
-          sushi2,
-          salad1,
-          salad2,
-          pasta1,
-          pasta2,
-          dessert1,
-          dessert2,
-          drink1,
-          drink2,
-          breakfast1,
-          breakfast2,
-        ];
-        return allImages[DateTime.now().microsecond % allImages.length];
+        // Если категория неизвестна, вернем первое подходящее изображение
+        if (category.contains('кур')) return burger;
+        if (category.contains('суп')) return burger;
+        if (category.contains('салат')) return burger;
+        return burger;
     }
   }
 }
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import '../services/gemini_service.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _messageController = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  final GeminiService _geminiService = GeminiService();
+  bool _isLoading = false;
+
+  void _sendMessage() async {
+    if (_messageController.text.isEmpty) return;
+
+    final message = _messageController.text;
+    _messageController.clear();
+
+    setState(() {
+      _messages.add(ChatMessage(text: message, isUser: true));
+      _isLoading = true;
+    });
+
+    final response = await _geminiService.sendMessage(message);
+
+    setState(() {
+      _messages.add(ChatMessage(text: response, isUser: false));
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Чат с ботом')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return Align(
+                  alignment:
+                      message.isUser
+                          ? Alignment.centerRight
+                          : Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 4),
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: message.isUser ? Colors.blue : Colors.grey[300],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Text(
+                      message.text,
+                      style: TextStyle(
+                        color: message.isUser ? Colors.white : Colors.black,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          if (_isLoading)
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: CircularProgressIndicator(),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: const InputDecoration(
+                      hintText: 'Введите сообщение...',
+                      border: OutlineInputBorder(),
+                    ),
+                    onSubmitted: (_) => _sendMessage(),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ChatMessage {
+  final String text;
+  final bool isUser;
+
+  ChatMessage({required this.text, required this.isUser});
+}
